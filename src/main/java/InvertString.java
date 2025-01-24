d1c3ae333488c7607015725dd8f309b401ecf29a
Darkest-White <darkest.white.fox@gmail.com>
2025-01-24 13:34:52 +0300

    Update

--- src/main/java/InvertString.java
+++ src/main/java/InvertString.java
@@ -3,11 +3,10 @@ import java.util.List;
 
 public class InvertString {
     public static String invert(String str) {
-        char[] arr = str.toCharArray();
         StringBuilder inverted = new StringBuilder();
 
-        for (int i = arr.length - 1; i >= 0; i--) {
-            inverted.append(arr[i]);
+        for (int i = str.length() - 1; i >= 0; i--) {
+            inverted.append(str.charAt(i));
         }
         return inverted.toString();
         //return new StringBuilder(str).reverse().toString();
